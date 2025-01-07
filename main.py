a8a7aee8dd5e1a7b94a61f3791313aee0609e2c8
soumen_biswas <biswassoumen208@gmail.com>
2025-01-07 17:53:37 +0600

    Turtle

---
+++ main.py
@@ -0,0 +1,10 @@
+import turtle
+tom = turtle.Turtle()
+nonte =turtle.Turtle()
+fonte =turtle.Turtle()
+tom.circle(100)
+nonte.shape("circle")
+fonte.shape("square")
+nonte.left(30)
+nonte.forward(100)
+fonte.backward(50)
